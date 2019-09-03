0b9dd0816195aff214c6470c393ce74a0160ed58
Open Eyes Consulting <nidhal.mrad@openeyesconsulting.com>
2019-09-03 14:20:05 +0100

    second branch

--- gitNidhal2/ViewController.swift
+++ gitNidhal2/ViewController.swift
@@ -3,7 +3,7 @@
 //  gitNidhal2
 //
 //  Created by Open Eyes Consulting on 9/3/19.
-//  Copyright © 2019 Open Eyes Consulting. All rights reserved.
+//  
 //
 
 import UIKit
@@ -12,8 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        print("hello")
+        
+        print("branch 2 ")
     }
 
 
