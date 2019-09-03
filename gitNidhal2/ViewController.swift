5c456cc3d0aa19d167ce6e352246ffcb8419c106
Open Eyes Consulting <nidhal.mrad@openeyesconsulting.com>
2019-09-03 12:06:27 +0100

    Initial Commit

---
+++ gitNidhal2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitNidhal2
+//
+//  Created by Open Eyes Consulting on 9/3/19.
+//  Copyright © 2019 Open Eyes Consulting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
