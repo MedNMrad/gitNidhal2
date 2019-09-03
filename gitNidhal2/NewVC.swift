61a97293008e3b0d2d1869f52aa2a1ede4f3ac2f
Open Eyes Consulting <nidhal.mrad@openeyesconsulting.com>
2019-09-03 12:10:50 +0100

    add new vienController and print

---
+++ gitNidhal2/NewVC.swift
@@ -0,0 +1,30 @@
+//
+//  NewVC.swift
+//  gitNidhal2
+//
+//  Created by Open Eyes Consulting on 9/3/19.
+//  Copyright © 2019 Open Eyes Consulting. All rights reserved.
+//
+
+import UIKit
+
+class NewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
